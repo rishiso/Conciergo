9de7a2a7d8c3033ddcfcfb2bc57b38661e8104dd
Rishi Soni <rishisoni.5678@gmail.com>
2022-08-18 11:12:15 -0400

    Update Maps.js

--- src/util/Maps.js
+++ src/util/Maps.js
@@ -1,10 +1,12 @@
 function convToMaps(address, city, state, zipCode) {
-    let a = address.replaceAll(" ", "+");
-    a += ",";
-    let c = city.replaceAll(" ", "+");
-    c += ",";
+    if (address !== null) {
+        let a = address.replaceAll(" ", "+");
+        a += ",";
+        let c = city.replaceAll(" ", "+");
+        c += ",";
 
-    return(`https://www.google.com/maps/search/?api=1&query=${a}+${c}+${state}+${zipCode}`);
+        return(`https://www.google.com/maps/search/?api=1&query=${a}+${c}+${state}+${zipCode}`);
+    }
 }
 
 export default convToMaps;
